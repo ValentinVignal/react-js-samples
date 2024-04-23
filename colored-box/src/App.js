489af2c07a66f4cf574123c1024b8a20f35e1178
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-04-23 23:41:47 +0800

    :sparkles: colored-box

--- colored-box/src/App.js
+++ colored-box/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  
+  const rgbToHex = (rgb) => {
+    if (!rgb) {
+      return "00";
+    }
+    const value = parseInt(rgb);
+    if (isNaN(value) || value < 0) {
+      return "00";
+    }
+    if (value >= 255) {
+      return "ff"
+    }
+    const hex = value.toString(16);
+    if (hex.length === 1) {
+      return `0${hex}`;
+    }
+    return hex;
+  }
+  
+  const rgbHasError = (rgb) => {
+    if (!rgb) return false;
+    const value = parseInt(rgb);
+    if (isNaN(value) || value < 0 || value > 255) {
+      return true;
+    }
+    const reg = /^\d+$/;
+    if (!reg.test(rgb)) {
+      return true;
+    }
+    return false;
+  }
+  
+  const [red, setRed] = useState("");
+  const onRedChange = (event) => {
+    setRed(event.target.value);
+  }
+    const [green, setGreen] = useState("");
+  const onGreenChange = (event) => {
+setGreen(event.target.value);
+  }
+    const [blue, setBlue]  = useState("");
+  const onBlueChange = (event) => {
+setBlue(event.target.value);
+  }
+
+  
+  const hex = `#${rgbToHex(red)}${rgbToHex(green)}${rgbToHex(blue)}`;
+  
+  const hasError = rgbHasError(red) || rgbHasError(green) || rgbHasError(blue);
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label id="redLabel">Red</label>
+      <input id="redColor" value={red} onChange={onRedChange} />
+            <label id="greenLabel">Green</label>
+      <input id="greenColor" value={green} onChange={onGreenChange} />
+            <label id="blueLabel">Blue</label>
+      <input id="blueColor" value={blue} onChange={onBlueChange} />
+      
+      {hasError ? <label id="warningMessage">Error</label> : <div></div>}
+      <div id="resultColor" style={{height: "100px", width: "100px", backgroundColor: hex}}/>
     </div>
-  );
+  )
 }
+
 
 export default App;
